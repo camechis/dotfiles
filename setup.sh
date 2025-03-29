53decd43c711524ac12c8245f3b0d78a3f78bc38
Travis Camechis <camechis@gmail.com>
2025-03-29 16:33:49 -0400

    initial install script to install basic packages

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# List of packages to install
+packages=(
+  "neovim"
+  "emacs"
+  # Add more packages as needed
+)
+
+# Loop through the packages and install them
+for package in "${packages[@]}"; do
+  if pacman -Qi "$package" &>/dev/null; then
+    echo "Package '$package' is already installed."
+  else
+    sudo pacman -S --noconfirm "$package"
+    if [ $? -eq 0 ]; then
+      echo "Package '$package' installed successfully."
+    else
+      echo "Failed to install package '$package'."
+    fi
+  fi
+done
+
+echo "Package installation process completed."
+
